aee193f1dbd09f0fadf267b207e927569c64cf50
Jan Tuziak <jan.tuziak@gmail.com>
2023-12-15 13:39:54 +0100

    add Bootstrap

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 import jinja2
+import json
 
-x = requests.get('https://api.justjoin.it/v2/user-panel/offers?categories[]=3&page=1&sortBy=published&orderBy=DESC&perPage=100&salaryCurrencies=PLN')
-print(x.status_code)
+headers = {'Version': '2'}
+x = requests.get('https://api.justjoin.it/v2/user-panel/offers?&page=1&sortBy=published&orderBy=DESC&perPage=100', headers=headers)
+offers = json.dumps(x.json(), indent=4)
+with open("offers.json", "w") as outfile:
+    outfile.write(offers)
